bcb09d2f8fe82c56724d6f530766da1d2a8eeaa6
ia <isaac.ardis@gmail.com>
2017-07-01 14:32:42 -0500

    :floppy_disk:

--- is_up.1h.sh
+++ is_up.1h.sh
@@ -6,16 +6,19 @@
 #
 # Gets the status of your website
 
+sleep 2m
 
 areteh_url='http://areteh.co/#/' # replace with your url
-rstacks_url='http://www.rstacks.org' # replace with your url
+rstacks_url='https://www.openbound.org' # replace with your url
+spoop_url="http://www.spoop.info"
 
 areteh_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $areteh_url)
 rstacks_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $rstacks_url)
+spoop_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $spoop_url)
 
 case "$areteh_code" in
 "200")
-    echo "•| color=green" # • 
+    echo "•| color=green" # •
     ;;
 "301" | "302")
     echo "•| color=yellow"
@@ -26,7 +29,7 @@ case "$areteh_code" in
 esac
 case "$rstacks_code" in
 "200")
-    echo "•| color=green" # • 
+    echo "•| color=green" # •
     ;;
 "301" | "302")
     echo "•| color=yellow"
@@ -36,10 +39,23 @@ case "$rstacks_code" in
     ;;
 esac
 
+case "$spoop_code" in
+"200")
+    echo "•| color=green" # •
+    ;;
+"301" | "302")
+    echo "•| color=yellow"
+    ;;
+*)
+    echo "•| color=red"
+    ;;
+esac
+
+
 echo "---"
 echo "$areteh_url -> $areteh_code"
 echo "$rstacks_url -> $rstacks_code"
-
+echo "$spoop_url -> $spoop_code"
 
 
 
